412139f3683365f3c8ab2755e6a7114187a5a18b
KOOJOOOOO3 <jojoalrashidii@gmail.com>
2026-07-08 13:29:20 +0300

    Update index7.js

--- index7.js
+++ index7.js
@@ -1,31 +1,34 @@
-const { Client, GatewayIntentBits, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require('discord.js');
+const { Client, GatewayIntentBits, ActionRowBuilder, ButtonBuilder, ButtonStyle, AttachmentBuilder } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 client.on('messageCreate', async (message) => {
     if (message.content === '!ماب') {
         
-        // تعريف الأزرار
+        // 1. تحضير ملف الصورة من جهازك
+        const file = new AttachmentBuilder('./map.jpeg'); // تأكد من مطابقة الاسم وامتداد الملف
+
+        // 2. تعريف الأزرار (يجب أن يكون الستايل Link لاستخدام setURL)
         const row = new ActionRowBuilder()
             .addComponents(
                 new ButtonBuilder()
                     .setLabel('Read Rules')
-                    .setStyle(ButtonStyle.Secondary)
-                    .setURL('https://discord.com/channels/@me/1013930692381507584'), // رابط السيرفر
+                    .setStyle(ButtonStyle.Link)
+                    .setURL('https://discord.com/channels/SERVER_ID/1013930692381507584'),
 
                 new ButtonBuilder()
                     .setLabel('Apply')
-                    .setStyle(ButtonStyle.Secondary)
-                    .setURL('https://discord.com/channels/@me/1512520871313408142'),
+                    .setStyle(ButtonStyle.Link)
+                    .setURL('https://discord.com/channels/SERVER_ID/1512520871313408142'),
 
                 new ButtonBuilder()
                     .setLabel('Arena')
-                    .setStyle(ButtonStyle.Secondary)
-                    .setURL('https://discord.com/channels/@me/1524314320379314297'),
+                    .setStyle(ButtonStyle.Link)
+                    .setURL('https://discord.com/channels/SERVER_ID/1524314320379314297'),
             );
 
-        // إرسال الصورة مع الأزرار
+        // 3. إرسال الصورة والأزرار
         await message.channel.send({
-            files: ['https://cdn.discordapp.com/attachments/1501300022808023351/1524355743451578388/IMG_8689.jpg?ex=6a4f726a&is=6a4e20ea&hm=ab53e31f2035dbd6d33520c4fc6d045559138f6e397c96150cbfe56843dd0f40'], // رابط الصورة 
+            files: [file],
             components: [row]
         });
     }
